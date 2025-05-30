d059d58442b06a033c22574b5ab3ddb8831cc846
Natalia <meryrua@gmail.com>
2025-05-30 11:51:34 -0400

    Add Dealer details. Now it's imposible to add review (no dropdown CarMake)

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -119,7 +119,7 @@ def get_dealer_details(request, dealer_id):
     if(dealer_id):
         endpoint = "/fetchDealer/"+ str(dealer_id)
         dealerships = get_request(endpoint)
-        return JsonResponse({"status":200,"dealers":dealerships})
+        return JsonResponse({"status":200,"dealer":dealerships})
     else:
         return JsonResponse({"status":400,"message":"Bad Request"})
 
--- server/frontend/src/components/Dealers/Dealer.jsx
+++ server/frontend/src/components/Dealers/Dealer.jsx
@@ -24,15 +24,17 @@ const Dealer = () => {
   let reviews_url = root_url+`djangoapp/reviews/dealer/${id}`;
   let post_review = root_url+`postreview/${id}`;
   
-  const get_dealer = async ()=>{
-    const res = await fetch(dealer_url, {
-      method: "GET"
-    });
+  const get_dealer = async () => {
+    const res = await fetch(dealer_url, { method: "GET" });
     const retobj = await res.json();
-    
-    if(retobj.status === 200) {
-      let dealerobjs = Array.from(retobj.dealer)
-      setDealer(dealerobjs[0])
+    console.log("Dealer data response:", retobj);
+  
+    if (res.ok && retobj.dealer) {
+      //console.log("Dealer :", retobj.dealer);
+      //console.log("Dealer :", retobj.dealer[0]["city"]);
+      setDealer(retobj.dealer[0]); 
+    } else {
+      console.error("Unexpected dealer data format:", retobj);
     }
   }
 
