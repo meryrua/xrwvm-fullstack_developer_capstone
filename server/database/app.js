a6dd6b917ab931160b5e0f44b387559e2b26005c
meryrua <meryrua@gmail.com>
2025-06-04 14:27:31 +0300

    Changes for linter

--- server/database/app.js
+++ server/database/app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const fs = require('fs');
 const  cors = require('cors');
-const app = express()'
+const app = express();
 const port = 3030;
 
 app.use(cors());
