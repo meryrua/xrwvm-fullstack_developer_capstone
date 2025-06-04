7f1ee608cc9cfa7c483220ba1b85e0e1bec6cb09
meryrua <meryrua@gmail.com>
2025-06-04 17:04:50 +0300

    Changes for linter

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -3,7 +3,6 @@ from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
 from . import views
-from .restapis import get_request, analyze_review_sentiments, post_review
 
 app_name = 'djangoapp'
 urlpatterns = [
@@ -22,10 +21,4 @@ urlpatterns = [
     path(route='dealer/<int:dealer_id>', view=views.get_dealer_details, name='get_dealer_by_id'),
     path(route='reviews/dealer/<int:dealer_id>', view=views.get_dealer_reviews, name='dealer_details'),
     path(route='add_review', view=views.add_review, name='add_review'),
-
-
-    # path for dealer reviews view
-
-    # path for add a review view
-
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
