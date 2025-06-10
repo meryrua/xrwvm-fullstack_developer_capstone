ebb1753c78a5ef9a703b0c84d45c9eab7da18d0e
Natalia <meryrua@gmail.com>
2025-06-10 08:12:36 -0400

    Fix bugs after linter

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -14,7 +14,7 @@ import logging
 import json
 from django.views.decorators.csrf import csrf_exempt
 from .populate import initiate
-from .restapis import get_request, analyze_review_sentiments
+from .restapis import get_request, analyze_review_sentiments, post_review
 
 from .models import CarMake, CarModel
 
@@ -138,9 +138,10 @@ def get_dealer_details(request, dealer_id):
 # Create a `add_review` view to submit a review
 def add_review(request):
     if (request.user.is_anonymous is False):
-        # data = json.loads(request.body)
+        data = json.loads(request.body)
         try:
-            # response = post_review(data)
+            response = post_review(data)
+            print(response)
             return JsonResponse({"status": 200})
         except Exception:
             return JsonResponse({"status": 401,
